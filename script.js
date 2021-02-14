b52a4a7d7a659b0a6937e43b3c19c2bbfc19ee48
thommccarthy <mccarttj@gmail.com>
2021-02-14 13:11:33 -0500

    added lodash for practice with npm, functionality not changed

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+var _ = require('lodash');
+
+let array1 = [1, 2, 3, 4, 5, 6, 7];
+console.log('answer:', _.without(array1, 7));
+
 var css = document.querySelector("h3"); 
 var color1 = document.querySelector(".color1"); 
 var color2 = document.querySelector(".color2");
